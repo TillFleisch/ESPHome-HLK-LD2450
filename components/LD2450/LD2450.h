c5ffb5b4aad97a1736e94dd85528fe7fff99f2a3
Till <till@fleisch.dev>
2025-12-13 12:58:45 +0100

    fix: add missing USE_BUTTON define guard

--- components/LD2450/LD2450.h
+++ components/LD2450/LD2450.h
@@ -72,14 +72,16 @@ namespace esphome::ld2450
     class BluetoothSwitch;
     class BaudRateSelect;
 
+#ifdef USE_BUTTON
     /**
      * @brief Empty button definition used as a template for the restart and factory reset buttons.
      */
     class EmptyButton : public button::Button
     {
     protected:
-        virtual void press_action(){};
+        virtual void press_action() {};
     };
+#endif
 
     /**
      * @brief UART component responsible for processing the data stream provided by the HLK-LD2450 sensor
